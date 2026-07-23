138ba5641fe4789e46a8e5052ac9d0dc0cbce140
prathameshrathod9890 <prathameshh.anantkamal@gmail.com>
2026-07-23 11:37:59 +0530

    update14_07_26

--- lib/api_services/urls.dart
+++ lib/api_services/urls.dart
@@ -1,10 +1,10 @@
 //const String baseUrl = "https://hobitadmin.anantkamalsoftwarelabs.com";
 //const String baseUrl = "https://backendhobit.anantkamalsoftwarelabs.com";
 
-const String baseUrl = "https://hobitadmin.hobit.club";
+//const String baseUrl = "https://hobitadmin.hobit.club";
 
 //const String baseUrl = "https://uatadmin.hobit.club";
-// const String baseUrl = "https://testing.hobit.club";
+ const String baseUrl = "https://testing.hobit.club";
 //registrations apis
 const String signUpUrl = "/api/worker/register_app";
 const String  serviceCategoriesUrl = "/api/service-categories";
